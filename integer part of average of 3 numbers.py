31a7c4aaf8b449277ad6fec066054a5c605ee8e1
jiaqichen2010 <72290140+jiaqichen2010@users.noreply.github.com>
2023-01-28 10:58:40 +0800

    Delete integer part of average of 3 numbers.py

--- integer part of average of 3 numbers.py
+++
@@ -1,9 +0,0 @@
-x = input("First number: ")
-y = input("Second number: ")
-z = input("Third number: ")
-
-total_Sum = int(x) + int(y) + int(z)
-average = total_Sum / 3
-int_average = average // 1
-
-print('The answer is ' + str(int(int_average)))
