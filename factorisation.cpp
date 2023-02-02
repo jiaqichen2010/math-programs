2f61d274d0b797c4c2d6af45d88c419a0561ff77
jiaqichen2010 <72290140+jiaqichen2010@users.noreply.github.com>
2023-02-02 20:32:08 +0800

    Update factorisation.cpp

--- factorisation.cpp
+++ factorisation.cpp
@@ -4,13 +4,22 @@ using namespace std;
 int main() {
 	long long n; cin >> n;
 	long long factor = 2;
+	while (n % 2 == 0) {
+		n /= factor;
+	}
+	factor += 1;
 	while (factor <= n) {
+		if (factor > floor(sqrt(n))) {
+			cout << n;
+			break;
+		}
 		if (n % factor == 0) {
 			cout << factor << " ";
 			n /= factor;
-			factor = 1;
+			factor -= 2;
 		}
-		factor += 1;
+		factor += 2;
+			
 	}
 	return 0;
 }
